982a221b54e95013af398a2776c009e3fe498dd1
Backslash1 <72407847+Backslash1@users.noreply.github.com>
2022-06-24 11:31:46 +0530

    Changed chi-sq python implementation
    Used contingency table to compute the statistics

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def home(request : Request):
     return templates.TemplateResponse("index.html" , {"request" : request})
 
 @app.api_route("/store_file" , methods=['POST'])
-def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...)):
     # Saves file in the predefined directory
     fname , ext = file.filename.split('.')
     # dir = "./workSpace/userData"
@@ -33,12 +33,12 @@ def upload_file(file: UploadFile = File(...)):
     return {"file_url" : file_url , "columns":columns_list}
 
 @app.api_route("/df_before_processing" , methods=["POST"])
-def data_before_processing(filepath: str = Body(...,embed=True)):
+async def data_before_processing(filepath: str = Body(...,embed=True)):
     df = pd.read_csv(filepath , nrows=5)
     return {df.to_json(orient="records")}
 
 @app.api_route("/existingFiles" , methods = ["GET"])
-def get_list_of_files():
+async def get_list_of_files():
     files_list = list()
     dir = os.path.join("workSpace" , "userData")
 
@@ -53,7 +53,7 @@ async def get_file_columns(filepath: str = Body(...,embed=True)):
     data = pd.read_csv(filepath)
     return {"columns" : data.columns.to_list()}
 @app.api_route("/target_columns" , methods=["POST"])
-def get_target_columns(filepath:str = Body(...,embed=True)):
+async def get_target_columns(filepath:str = Body(...,embed=True)):
     # Returns possible target columns list
     data = pd.read_csv(filepath)
     numerics = ['int16', 'int32', 'int64']
@@ -66,13 +66,13 @@ async def get_analysis(method : str = Body(...,embed=True) , filepath : str = Bo
     kval = int(kval)
 
     if method.lower() == "chi-square":
-        return __chi_square__(filepath , target , kval)
+        return await __chi_square__(filepath , target , kval)
     elif method.lower() == "information gain":
-        return __information_gain__(filepath , target , kval)
+        return await __information_gain__(filepath , target , kval)
     elif method.lower() == "fisher":
-        return __fisher__(filepath , target , kval)
+        return await __fisher__(filepath , target , kval)
     elif method.lower() == "correlation coefficient":
-        return __correlation_coefficient__(filepath , target , kval)
+        return await __correlation_coefficient__(filepath , target , kval)
     else:
         raise HTTPException(
             status_code = 404,
@@ -80,29 +80,62 @@ async def get_analysis(method : str = Body(...,embed=True) , filepath : str = Bo
         )
 
 # Feature Selection Methods Implementation
-def __chi_square__(filepath , target , kval):
+async def __chi_square__(filepath , target , kval):
     from sklearn.feature_selection import chi2
-    
+    from scipy.stats import chi2_contingency
+
     data = pd.read_csv(filepath)
     X = data.drop(target , axis=1)
+    # print(data.head())
     y = data[target]
-    f_score = chi2(X,y)
 
-    #First array will return f scores and second array will return p scores
+    ### new implementation
 
-    p_values = pd.Series(f_score[1] , index=X.columns)
-    p_values.sort_values(ascending = False , inplace =True)
+    feature_cols = X.columns
+    p_value_list = list()
+    statvalue_list = list()
+    for col in feature_cols:
+        contingency_table = pd.crosstab(X[col] , y)
+        contingency_table_val = contingency_table.values
+        print(contingency_table)
+        stat, pvalue, dof, _ = chi2_contingency(contingency_table_val)
+        p_value_list.append(pvalue)
 
-    k_best_feature = p_values.head(kval).index.to_list()
-    #This will select the k best features from the data set.
+    p_values_df = pd.Series(p_value_list , index=feature_cols)
+    p_values_df.sort_values(ascending = True , inplace =True)
+    print(p_values_df.head())
+    k_best_feature = p_values_df.head(kval).index.to_list()
 
-    v1 = p_values.index.to_list()
-    v2 = p_values.to_list()
-    print(v1)
-    print(v2)
+    v1 = p_values_df.index.to_list()
+    v2 = p_values_df.to_list()
+    ###
+    # f_score = chi2(X,y)
+    # print(X.columns)
+    # print(f_score[0])
+    # #First array will return f scores and second array will return p scores
+
+    # p_values = pd.Series(f_score[1] , index=X.columns)
+    # p_values.sort_values(ascending = True , inplace =True)
+
+    # k_best_feature = p_values.head(kval).index.to_list()
+    # #This will select the k best features from the data set.
+
+    # v1 = p_values.index.to_list()
+    # v2 = p_values.to_list()
+    # print(v1)
+    # print(v2)
+
+    # stat_val = pd.Series(f_score[0] , index=X.columns)
+    # stat_val.sort_values(ascending=False , inplace=True)
+    # print(stat_val)
+
+    # k_best_feature = stat_val.head(kval).index.to_list()
+
+    # v1 = stat_val.index.to_list()
+    # v2 = stat_val.to_list()
     return {"labels" : v1 , "values": v2 , "best-features" : k_best_feature}
 
-def __information_gain__(filepath , target , kval):
+async def __information_gain__(filepath , target , kval):
     from sklearn.feature_selection import mutual_info_classif
 
     data = pd.read_csv(filepath)
@@ -110,10 +143,8 @@ def __information_gain__(filepath , target , kval):
     y = data[target]
     mutual_info = mutual_info_classif(X, y)
     mutual_info = pd.Series(mutual_info)
-    print("mutualkl")
 
     mutual_info.index = X.columns
-    print(mutual_info)
     mutual_info.sort_values(ascending=False,inplace=True)
 
     v1 = mutual_info.index.tolist()
@@ -122,12 +153,10 @@ def __information_gain__(filepath , target , kval):
 
     return {"labels" : v1 , "values": v2 , "best-features" : ans}
 
-def __correlation_coefficient__(filepath , target , kval):
+async def __correlation_coefficient__(filepath , target , kval):
     data = pd.read_csv(filepath)
 
     corr_mat = data.corr()
-    print("correlation")
-    print(corr_mat)
     cor_target = abs(corr_mat[target])
     cor_target = cor_target.sort_values(ascending=False)
 
@@ -138,24 +167,17 @@ def __correlation_coefficient__(filepath , target , kval):
     # mat_val = corr_mat.values
     
     N = len(corr_mat.values)
-    print(N)
     mat_val = []
-    print(mat_val)
     for i in range(N - 1 , -1 , -1):
-        print(f"i:{i}" , end="")
         mat_val.append([])
         for j in range(N):
             if i >= j:
                 # upper triangle
                 mat_val[N - i - 1].append(corr_mat.values[i][j])
 
-    # print(mat_val)
-    for i in range(N):
-        print(mat_val[i])
-    # mat_val = mat_val.tolist()
     return {"best-features":kbest_features_col ,"best-features-val":kbest_features_val ,"corr_mat" : mat_val}
 
-def __fisher__(filepath , target , kval):
+async def __fisher__(filepath , target , kval):
     from skfeature.function.similarity_based import fisher_score
     
     data = pd.read_csv(filepath)
@@ -164,28 +186,20 @@ def __fisher__(filepath , target , kval):
     nX = X[X.columns.to_list()].to_numpy()
     ny = y.to_numpy()
     
-    print("before")
     ranks = fisher_score.fisher_score(nX , ny)
-    print("after")
-    print(data.columns[0 : len(data.columns) - 1])
     
     feat_imp = pd.Series(ranks , X.columns)
     feat_imp = feat_imp.sort_values(ascending=False)
     kbest_feature_col = feat_imp.index.tolist()
     kbest_feature_val = feat_imp.values.tolist()
-    print(feat_imp)
-    print(kbest_feature_col)
-    print(kbest_feature_val)
 
     return {"best-features":kbest_feature_col[:kval] , "labels" : kbest_feature_col , "values": kbest_feature_val}
 # Helper functions
-def _save_file_to_disk(uploaded_file , path = "." , save_as = "default"):
+async def _save_file_to_disk(uploaded_file , path = "." , save_as = "default"):
     # saves file at the given path and returns its complete URL
 
     extension = os.path.splitext(uploaded_file.filename)[-1]
-    print(f"extension: {extension}")
     temp_file = os.path.join(path , save_as + extension)
-    print(f"tempfile {temp_file}")
     with open(temp_file , "wb") as buffer:
         shutil.copyfileobj(uploaded_file.file , buffer)
     return temp_file
